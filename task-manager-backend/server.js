26de291d5e9cfd17c7960e71e8aa265b4bc6b77f
Shiva143-debug <64155226+Shiva143-debug@users.noreply.github.com>
2024-05-01 18:02:27 +0530

    Update server.js

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -1,3 +1,7 @@
+// I have used glitch for backend only tested in local server
+
+//here the glitch code
+
 
 const express = require('express');
 // const mysql = require('mysql');
@@ -13,12 +17,7 @@ app.use(cors())
 app.use(express.json())
 // app.use(bodyParser.json());
 
-// const pool = mysql.createConnection({
-//     host: "148.72.246.179",
-//     user: "shaik",
-//     password: "",
-//     database: 'test'
-// })
+
 const pool = new Pool({
     user: 'oss_admin',
     host: '148.72.246.179',
@@ -75,6 +74,80 @@ app.put("/updatetask", (req, res) => {
     });
   });
 
-app.listen(3005, () => {
-    console.log("listening on 3005")
-})
+app.listen(process.env.PORT || 4000,()=>console.log("server on "+process.env.PORT))
+
+
+
+// const express = require('express');
+// // const mysql = require('mysql');
+// const { Pool } = require('pg');
+// const cors = require('cors')
+// const bodyParser = require('body-parser');
+
+
+
+
+// const app = express()
+// app.use(cors())
+// app.use(express.json())
+// // app.use(bodyParser.json());
+
+// const pool = new Pool({
+//     user: 'oss_admin',
+//     host: '148.72.246.179',
+//     database: 'expense',
+//     password: 'Latitude77',
+//     schema:"public",
+//     port: '5432', 
+// });
+
+
+// app.post("/addtasks", (req, res) => {
+//     const {name, status, assigee } = req.body;
+//     console.log("Received data:", req.body);
+
+//     const sql = "INSERT INTO tasks (name, status, assigee) VALUES ($1, $2, $3)";
+//     const values = [name, status, assigee];
+//     console.log(values)
+
+//     pool.query(sql, values, (err, result) => {
+//         if (err) return res.json(err)
+//         return res.json(result)
+//         console.log(result)
+
+//     })
+// })
+
+
+// app.get('/gettasks', (req, res) => {
+
+
+//     const sql = "SELECT * from tasks";
+//     pool.query(sql,(err, data) => {
+//         console.log(err);
+//         console.log(data);
+//         if (err) return res.json(err);
+//         return res.json(data.rows)
+//     })
+// })
+
+// app.put("/updatetask", (req, res) => {
+   
+//     const {status,taskId } = req.body;
+  
+//     const sql = "UPDATE tasks SET status = $1 WHERE id = $2";
+//     const values = [status, taskId];
+//     console.log(values)
+  
+//     pool.query(sql, values, (err, result) => {
+//       if (err) {
+//         console.error('Error updating task:', err);
+//         return res.status(500).json({ error: 'Error updating task' });
+//       }
+//       return res.json({ message: 'Task updated successfully' });
+//     });
+//   });
+
+// app.listen(3005, () => {
+//     console.log("listening on 3005")
+// })
